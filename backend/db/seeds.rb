b9c06a834334226b2f7551f861cd58dc1dd25455
cernanb <cernanb@gmail.com>
2020-08-20 21:31:45 -0700

    Add seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+  c = Company.create( name: Faker::Company.name, 
+                      location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}"
+                    )
+
+  3.times do 
+    c.jobs.create( job_type: rand(0..2), 
+                   title: Faker::Job.title, 
+                   description: Faker::Lorem.paragraph(sentence_count: 2)
+                 )
+  end
+end
