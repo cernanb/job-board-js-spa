e1e414d1b0c29ac07c649fd5dae6e6005b821ba5
cernanb <cernanb@gmail.com>
2020-08-20 21:30:43 -0700

    Update schema

--- backend/app/models/job.rb
+++ backend/app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :company
+
+  enum status: [ :full_time, :part_time, :contract ]
 end
--- backend/db/migrate/20200821040542_create_jobs.rb
+++ backend/db/migrate/20200821040542_create_jobs.rb
@@ -1,8 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
-      t.integer :type
-      t.string :company
+      t.integer :job_type
       t.string :title
       t.string :description
       t.references :company, null: false, foreign_key: true
